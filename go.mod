719b178b160e7f69b722e9615dbaf03356707f79
ilyassjaatit <ilyassjaatit>
2022-04-23 19:49:32 +0200

    add env file

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/jlaffaye/ftp v0.0.0-20220310202011-d2c44e311e78 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,20 +2,28 @@ package main
 
 import (
 	"fmt"
+	"github.com/jlaffaye/ftp"
+	"github.com/joho/godotenv"
 	"log"
+	"os"
 	"reflect"
 	"time"
-
-	"github.com/jlaffaye/ftp"
 )
 
 func main() {
-	c, err := ftp.Dial("test_server_ftp", ftp.DialWithTimeout(5*time.Second))
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	ftpHost := os.Getenv("FTP_HOST")
+	ftpPort := os.Getenv("FTP_PORT")
+	c, err := ftp.Dial(ftpHost+":"+ftpPort, ftp.DialWithTimeout(5*time.Second))
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	err = c.Login("test_user", "test_password")
+	ftpUser := os.Getenv("FTP_USER")
+	ftpPAssword := os.Getenv("FTP_PASSWORD")
+	err = c.Login(ftpUser, ftpPAssword)
 	if err != nil {
 		log.Fatal(err)
 	}
